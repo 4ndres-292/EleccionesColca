a6055f0e56fc014020a5b9e7985fe551c078782c
4ndres-292 <choquecahuanaandresoriginal@gmail.com>
2026-03-09 18:38:13 -0400

    sprint 1:houses module basic API

--- server.js
+++ server.js
@@ -18,6 +18,38 @@ app.get("/", async (req, res) => {
     }
 });
 
+app.post("/houses", async (req, res) => {
+    try {
+        const { house_code, owner_name, order_position } = req.body;
+
+        const result = await pool.query(
+            `INSERT INTO houses (house_code, owner_name, order_position)
+             VALUES ($1, $2, $3)
+             RETURNING *`,
+            [house_code, owner_name, order_position]
+        );
+
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.get("/houses", async (req, res) => {
+    try {
+        const result = await pool.query(
+            `SELECT * FROM houses
+             ORDER BY order_position ASC`
+        );
+
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
+});
+
 // Puerto dinámico: Render asigna process.env.PORT, si no existe usamos 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
